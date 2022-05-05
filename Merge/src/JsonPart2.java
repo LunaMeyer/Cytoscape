f90b971e6e97236cf55e25862b4c8f15aa27431b
Louis Montagne <102073161+Rui-Yama@users.noreply.github.com>
2022-05-05 22:30:13 +0200

    Add files via upload
    Le code principal du merge

---
+++ Merge/src/JsonPart2.java
@@ -0,0 +1,99 @@
+import java.util.*;
+import java.util.Map.Entry;
+import java.io.*;
+
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.JsonToken;
+public class JsonPart2 {
+	private JsonFactory factory = new JsonFactory();
+	private File src;
+	private JsonParser parser;
+	String tokenValue = null;
+	private  Map<String, List<String>> edges = new HashMap<String,List<String>>();
+	private int i=0;
+	
+	
+	public JsonPart2(String pathtofile, List<String> id){
+		this.src = new File(pathtofile);
+		try {
+			parser = factory.createJsonParser(src);
+			parser.nextToken();
+			
+			while (parser.nextToken()!=null) {
+				JsonToken token = parser.getCurrentToken();
+				if (token.equals(JsonToken.START_ARRAY)){
+					tokenValue= parser.getCurrentName();
+					if ("edges".equalsIgnoreCase(tokenValue)) {
+						System.out.println("Fonctionne");
+						step1(parser,id);
+					}
+				}
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Edges ajoutés : "+String.valueOf(i));
+	}
+	
+	private void step1(JsonParser parser, List<String> id) {
+		String tokenValue = null;
+		try {
+			while (parser.nextToken()!=null) {
+				tokenValue = parser.getCurrentName();
+				if ("data".equalsIgnoreCase(tokenValue)) {
+					inData(parser, id);
+				}
+			}
+		}
+		catch (IOException e) {
+				e.printStackTrace();
+			}
+	}
+		
+	private void inData(JsonParser parser, List<String> id) {
+		JsonToken token3;
+		String tokenValue = null, key = null;
+		List<String> temp = new ArrayList<String>();
+		int ii = 0;
+
+		
+		try {
+			while (parser.nextToken()!=null) {
+				token3 = parser.getCurrentToken();
+				if (token3.equals(JsonToken.END_OBJECT)) {
+					if(ii == 0) {
+					return;}
+					else {
+						edges.put(key, temp);
+						ii=0;
+						i++;
+						return;}
+				} 
+				else if("id".equals(parser.getCurrentName())){
+					parser.nextToken();
+					key = parser.getText();}
+				else if("source".equals(parser.getCurrentName())) {
+						parser.nextToken();
+						tokenValue=parser.getText();
+						temp.add(tokenValue);
+						if(id.contains(tokenValue)){ii++;}
+						}
+				else if("target".equals(parser.getCurrentName())) {
+						parser.nextToken();
+						tokenValue=parser.getText();
+						temp.add(tokenValue);
+						}
+				else if("stringdb_score".equals(parser.getCurrentName())) {
+					parser.nextToken();
+					tokenValue=parser.getText();
+					temp.add(tokenValue);
+					}
+				}
+			}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Merge/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.Map.Entry;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ExcelPart result = new ExcelPart("C:\\Users\\louis\\Desktop\\Network\\Elysia_Bioscience_Data_Res.xlsx");
+		List<String> keys = result.getKeys();	
+		JsonPart parser = new JsonPart("C:\\Users\\louis\\Desktop\\Network\\Homo-spaiens.cyjs",keys);
+		List<String> id = parser.getId();
+		JsonPart2 parser2 = new JsonPart2("C:\\Users\\louis\\Desktop\\Network\\Homo-spaiens.cyjs",id);
+		Map<String, String> nodes = parser.getNodes(); 
+		Map<String, List<String>> data = result.getData();
+		Map<String, List<String>> merge = mergeMap(data,nodes);
+		System.out.println("Finito");
+		for(Entry<String, List<String>> entry : merge.entrySet()) {
+			String size = String.valueOf(entry.getValue().size());
+			System.out.println(entry.getKey()+" : "+size +" "+entry.getValue());
+		}
+	}
+	public static Map<String, List<String>>  mergeMap(Map<String,List<String>> map1, Map<String, String> map2){
+		Map<String, List<String>> mapmerge = new HashMap<String,List<String>>();
+		List<String> templist = new ArrayList<String>();
+		String tempkey = new String();
+		
+		for(Entry<String, String> entry : map2.entrySet()) {
+			
+			tempkey = entry.getKey();
+			templist = map1.get(tempkey);
+			templist.add(entry.getValue());
+			mapmerge.put(tempkey, templist);
+		}
+		
+		return mapmerge;
+	}
+}
